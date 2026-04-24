da751cfaefc5c0a4d69f9eb7cfac49b1119e5e80
Samara Carvalho <rm573666@fiap.com.br>
2026-04-24 12:18:20 -0300

    Adcionei os cases 3, 4, 5 e 6

--- src/main.c
+++ src/main.c
@@ -4,10 +4,9 @@ int main() {
   int opcao;
   float nota1, nota2, media;
 
-}
 printf("==============================\n");
- printf("     SISTEMA DAS LULUZINAS     \n");
- printf("==============================\n");
+printf("     SISTEMA DAS LULUZINAS     \n");
+printf("==============================\n");
 
 printf("1 - Inserir notas\n");
 printf("2 - Calcular media\n");
@@ -24,10 +23,38 @@ case1:
 printf("Digite a nota 1: ");
 
 scanf("%f", %nota)
-printf ("Digite a nota 2: ");
+printf("Digite a nota 2: ");
 
+case 3:
+if(media >= 7.0){
+printf("Situacao: Aprovado! \n");
+}else if(media >= 5.0){ 
+printf("Situacao: Recuperação! \n");
+}
+break;
+
+case 4:
+printf("----Resultados----\n");
+printf("Notas: %.1f e %.1f\n", nota1, nota2);
+printf("Media Final: %.1f\n", media);
+break;
+
+case 5:
+printf("Funcionalidade de Derivada. \n");
+break;
+
+case 6:
+printf("Saindo do sistema....\n");
+brek;
+
+default:
+printf("Opcao invalida! Tente novamente.\n");
+}
+
+}
 
 
  
     return 0;
 }
+ 
