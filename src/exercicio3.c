12d37912eb2623f63c1aa75715c19e6840d383f4
Kethelyn de Oliveira Rocha <kethelyn.oliver.rocha@gmail.com>
2026-04-24 12:39:14 -0300

    adicionei o calculo da derivada

--- src/exercicio3.c
+++ src/exercicio3.c
@@ -3,8 +3,15 @@
 int main(){
 
 int a, b, c;
+printf("digite o valor de a: ");
+scanf("%d", &a);
+printf("digite o valor de b: ");
+scanf("%d", &b);
+printf("digite o valor de c: ");
+scanf("%d", &c);
 
-
+//calculo da derivada
+int coef_derivada = 2 * a;
 
   return 0;
 }
