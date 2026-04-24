f41b3e98a029d2f3cb53e63648a87c5d7b304ba9
Kethelyn de Oliveira Rocha <kethelyn.oliver.rocha@gmail.com>
2026-04-24 11:17:29 -0300

    adicionamos tres condicionais ao codigo

--- src/main.c
+++ src/main.c
@@ -9,6 +9,16 @@ int main() {
       scanf("%f", &nota2);
 
     media = nota1 + nota2 / 2; 
+
+    if (media >= 6 && media <=10){
+      printf("O aluno esta aprovado, com a media igual a: ", media );
+
+    }else if(media < 6 && media >= 4){
+      printf("O aluno esta de recuperacao, com a media:", media );
+    
+    }else{ 
+      printf("O aluno esta retido");
+    }
     
     return 0;
 }
