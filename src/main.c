a5a97d91ad6110f0c7ba33d9d2dba1dfd73c8d03
Gabriela Batista <batistagabi351@gmail.com>
2026-04-24 11:58:56 -0300

    Requisitei as nota 1 do usuario

--- src/main.c
+++ src/main.c
@@ -1,24 +1,41 @@
 #include<stdio.h>
-// Codicionais
+
 int main() {
-    float nota1, nota2, media;
-      printf("Digite a primeira nota: ");
-      scanf("%f", &nota1);
+  int opcao;
+  float nota1, nota2, media;
 
-      printf("Digite a segunda nota: ");
-      scanf("%f", &nota2);
+}
+printf("==============================\n");
+ printf("     SISTEMA DAS LULUZINAS     \n");
+ printf("==============================\n");
 
-    media = nota1 + nota2 / 2; 
+printf("1 - Inserir notas\n");
+printf("2 - Calcular media\n");
+printf("3 - Verificar situacao\n");
+printf("4 - Exibir resultado\n");
+printf("5 - Calcular derivada\n");
+printf("4 - Sair\n");
+printf("Escolha uma opção: ");
+scanf("%d", &opcao);
 
-    if (media >= 6 && media <=10){
-      printf("O aluno esta aprovado, com a media igual a: ", media );
+switch (opcao){
 
-    }else if(media < 6 && media >= 4){
-      printf("O aluno esta de recuperacao, com a media:", media );
-    
-    }else{ 
-      printf("O aluno esta retido");
-    }
-    
+case1:
+printf("Digite a nota 1: ");
+
+scanf("%f", %nota)
+printf ("Digite a nota 2: ")
+
+case 2:
+ if (nota1 && nota2){
+   media = (nota1+ nota2) / 2
+printf ("Media calculada com sucesso!")
+ }
+ else {
+  printf ("Erro: Insira as notas primeiro (opcao 1)\n");
+ }
+} break;
+
+case 3 
     return 0;
 }
